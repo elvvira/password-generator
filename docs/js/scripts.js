7458bb66d35bab30fbd5b97007fe7d38ed60e970
Daniel Jiménez <elvvira.castellanos@gmail.com>
2023-01-12 15:00:33 +0100

    terminado (a medias)

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -3,36 +3,95 @@ const lenghtElement = document.getElementById('lenght');
 const buttonElement = document.getElementById('button');
 const rangeElement = document.getElementById('range');
 const passwordElement = document.getElementById('password');
+const uppercaseCheck = document.getElementById('uppercase');
+const lowercaseCheck = document.getElementById('lowercase');
+const numbersCheck = document.getElementById('numbers');
+const symbolsCheck = document.getElementById('symbols');
+const strenght = document.getElementById('strenght');
+const allCheckBoxes = document.querySelectorAll('input[type="checkbox"]');
 
 let valueLenght = 0;
+let charactersAllowed = '';
+let passwordFinal = '';
+
+const abc = 'abcdefghijklmnopqrstuvwxyz';
+const abcUpper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const number = '1234567890';
+const symbols = '!"·$%&/()=?¿*^Ç¨_';
+
+const filterCheckbox = () => {
+  const checkboxChecked = [];
+  for (const checkBox of allCheckBoxes) {
+    if (checkBox.checked) {
+      checkboxChecked.push(checkBox);
+    }
+  }
+  return checkboxChecked;
+};
+
+const generateWord = () => {
+  passwordFinal = '';
+  for (let index = 0; index < valueLenght; index++) {
+    const randomPassword = Math.floor(Math.random() * charactersAllowed.length);
+    passwordFinal += charactersAllowed.charAt(randomPassword);
+  }
+  // console.log(passwordFinal);
+};
+
+formElement.addEventListener('change', event => {
+  if (event.target.id === 'range') {
+    lenghtElement.textContent = event.target.value;
+    valueLenght = event.target.value;
+  }
+
+  charactersAllowed = '';
+
+  if (uppercaseCheck.checked) {
+    charactersAllowed += abcUpper;
+  }
+  if (lowercaseCheck.checked) {
+    charactersAllowed += abc;
+  }
+  if (numbersCheck.checked) {
+    charactersAllowed += number;
+  }
+  if (symbolsCheck.checked) {
+    charactersAllowed += symbols;
+  }
+
+  passwordElement.textContent = charactersAllowed;
+
+  if (rangeElement.value <= 5) {
+    strenght.textContent = 'TOO SHORT';
+    buttonElement.setAttribute('disabled', true);
+  } else {
+    strenght.textContent = 'TOO WEAK';
+    buttonElement.removeAttribute('disabled');
+  }
+
+  if (rangeElement.value > 5 && filterCheckbox().length === 0) {
+    strenght.textContent = 'NO OPTIONS CHECKED';
+    buttonElement.setAttribute('disabled', true);
+  } else {
+    buttonElement.removeAttribute('disabled');
+  }
+
+  if (filterCheckbox().length === 1) {
+    strenght.textContent = 'TOO WEAK';
+  } else if (filterCheckbox().length === 2) {
+    strenght.textContent = 'WEAK';
+  } else if (filterCheckbox().length === 3) {
+    strenght.textContent = 'MEDIUM';
+  } else if (filterCheckbox().length === 4) {
+    strenght.textContent = 'STRENGTH';
+  }
+});
+
+buttonElement.addEventListener('click', e => {
+  generateWord();
+});
 
 formElement.addEventListener('submit', event => {
   event.preventDefault();
-  
+  passwordElement.value = passwordFinal;
 });
-
-formElement.addEventListener('change', event => {
-  lenghtElement.textContent = event.target.value;
-  valueLenght = event.target.value;
-  generateWord(valueLenght);
-});
-
-const generateWord = value => {
-  const aleatory = []
-  const abc = 'abcdefghijklmnopqrstuvwxyz'
-
-  for (let index = 0; index < value; index++) {
-    const randomLetter = Math.floor(Math.random()* 27)
-    aleatory.push(abc.charAt(randomLetter))
-  }
-  console.log(aleatory)
-
-  passwordElement.textContent = aleatory
-};
-
-
-buttonElement.addEventListener('click', e => {
-  
-})
-
-
